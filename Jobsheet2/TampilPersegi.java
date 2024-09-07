bb22f20a9b8181daa867d7425f0c42f71c89fd86
TyaraKusumaNevada <tyaranevada29@gmail.com>
2024-09-07 10:15:56 +0700

    Tugas Jobsheet 2

---
+++ Jobsheet2/TampilPersegi.java
@@ -0,0 +1,13 @@
+package Jobsheet2;
+
+public class TampilPersegi {
+    public static void main(String[] args) {
+        Persegi persegi = new Persegi(5);
+
+        persegi.dataPersegi(); //tapil data persegi (panjang persegi)
+
+        System.out.println("Luas persegi: " + persegi.luasPersegi()); // hitung dan tampil luas perseg
+        System.out.println("Keliling persegi: " + persegi.kelilingPersegi()); //hitung dan tmpil kelilng persegi
+    }
+}
+
