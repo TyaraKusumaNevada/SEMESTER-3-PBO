309c6c42121bbce4e427b19205aa37bd2b091348
TyaraKusumaNevada <tyaranevada29@gmail.com>
2024-09-19 15:26:41 +0700

    modifikasi js 3 percobaan 1

--- Jobsheet3/MotorDemo.java
+++ Jobsheet3/MotorDemo.java
@@ -1,10 +1,24 @@
 package Jobsheet3;
-
 public class MotorDemo {
+    
     public static void main(String[] args) {
         Motor motor = new Motor();
         motor.printStatus();
-        motor.kecepatan = 50;
+        motor.tambahKecepatan();
+
+        motor.nyalakanMesin();
         motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.matikanMesin();
+        motor.printStatus()
     }
 }
