748e469f000271ddde97364a81d37bc589d24b57
TyaraKusumaNevada <tyaranevada29@gmail.com>
2024-10-07 20:29:18 +0700

    percobaan 1 js 6

---
+++ Jobsheet6/ClassB.java
@@ -0,0 +1,14 @@
+package Jobsheet6;
+
+public class ClassB {
+    public int z;
+    
+    public void getNilaiZ (){
+        System.out.println("nillai z:"+ z);
+    }
+
+    public void getJumlah (){
+        System.out.println("jumlah :"+ (x+y+z));
+    }
+}
+
